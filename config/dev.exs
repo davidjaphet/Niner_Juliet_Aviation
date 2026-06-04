924c4c70bd7ada46be625d5a7170566101121365
david japhet musonda <davidjaphetmu0@gmail.com>
2026-06-04 10:07:50 +0200

    dev

--- config/dev.exs
+++ config/dev.exs
@@ -10,20 +10,12 @@ config :niner_juliet_aviation, NinerJulietAviation.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we can use it
-# to bundle .js and .css sources.
 config :niner_juliet_aviation, NinerJulietAviationWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4001],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: "H7dk/hlARwcXkeuQZpR6fqjyeQIRJ3T2CysSFYwn5SmOKk9YFkyObQqG7P7ZmWY8",
+  secret_key_base: System.get_env("SECRET_KEY_BASE") || raise("missing SECRET_KEY_BASE env var"),
   watchers: [
     esbuild:
       {Esbuild, :install_and_run, [:niner_juliet_aviation, ~w(--sourcemap=inline --watch)]},
@@ -54,9 +46,7 @@ config :phoenix, :stacktrace_depth, 20
 config :phoenix, :plug_init_mode, :runtime
 
 config :phoenix_live_view,
-  # Include HEEx debug annotations as HTML comments in rendered markup
   debug_heex_annotations: true,
-  # Enable helpful, but potentially expensive runtime checks
   enable_expensive_runtime_checks: true
 
 # Disable swoosh api client as it is only required for production adapters.
